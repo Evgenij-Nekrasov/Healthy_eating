08f158049fd6d2bb6bcf70e7323001840eac36e4
Evgeniy <137310332+Evgenij-Nekrasov@users.noreply.github.com>
2023-10-15 19:44:43 +0300

    Add files via upload

--- js/modules/forms.js
+++ js/modules/forms.js
@@ -1,3 +1,59 @@
+<<<<<<< HEAD
+import { closeModal, openModal } from "./modal";
+import { postData } from "../services/services";
+function forms(formsSelector, modalTimerId) {
+   const forms = document.querySelectorAll(formsSelector);
+   const message = {
+      loading: "img/form/spinner.svg",
+      success: "Thanks! We will contact you soon",
+      failure: "Something went wrong...",
+   };
+
+   forms.forEach((item) => {
+      bindPostData(item);
+   });
+
+   function bindPostData(form) {
+      form.addEventListener("submit", (e) => {
+         e.preventDefault();
+
+         let statusMessage = document.createElement("img");
+         statusMessage.src = message.loading;
+         statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+            `;
+         form.insertAdjacentElement("afterend", statusMessage);
+
+         const formData = new FormData(form);
+
+         const json = JSON.stringify(Object.fromEntries(formData.entries()));
+
+         postData("http://localhost:3000/requests", json)
+            .then((data) => {
+               console.log(data);
+               showThanksModal(message.success);
+               statusMessage.remove();
+            })
+            .catch(() => {
+               showThanksModal(message.failure);
+            })
+            .finally(() => {
+               form.reset();
+            });
+      });
+   }
+
+   function showThanksModal(message) {
+      const prevModalDialog = document.querySelector(".modal__dialog");
+
+      prevModalDialog.classList.add("hide");
+      openModal(".modal", modalTimerId);
+
+      const thanksModal = document.createElement("div");
+      thanksModal.classList.add("modal__dialog");
+      thanksModal.innerHTML = `
+=======
 import {closeModal, openModal} from './modal';
 import {postData} from '../services/services';
 function forms(formsSelector, modalTimerId) {
@@ -50,11 +106,25 @@ function forms(formsSelector, modalTimerId) {
         const thanksModal = document.createElement('div');
         thanksModal.classList.add('modal__dialog');
         thanksModal.innerHTML = `
+>>>>>>> 350eb14 (Add files via upload)
             <div class="modal__content">
                 <div class="modal__close" data-close>×</div>
                 <div class="modal__title">${message}</div>
             </div>
         `;
+<<<<<<< HEAD
+      document.querySelector(".modal").append(thanksModal);
+      setTimeout(() => {
+         thanksModal.remove();
+         prevModalDialog.classList.add("show");
+         prevModalDialog.classList.remove("hide");
+         closeModal(".modal");
+      }, 4000);
+   }
+}
+
+export default forms;
+=======
         document.querySelector('.modal').append(thanksModal);
         setTimeout(() => {
             thanksModal.remove();
@@ -66,3 +136,4 @@ function forms(formsSelector, modalTimerId) {
 }
 
 export default forms;
+>>>>>>> 350eb14 (Add files via upload)
--- js/script.js
+++ js/script.js
@@ -1,3 +1,46 @@
+<<<<<<< HEAD
+import "../css/styles.css";
+require("es6-promise").polyfill();
+import "nodelist-foreach-polyfill";
+
+import tabs from "./modules/tabs";
+import modal from "./modules/modal";
+import timer from "./modules/timer";
+import cards from "./modules/cards";
+import calc from "./modules/calc";
+import forms from "./modules/forms";
+import slider from "./modules/slider";
+import { openModal } from "./modules/modal";
+
+window.addEventListener("DOMContentLoaded", function () {
+   const modalTimerId = setTimeout(
+      () => openModal(".modal", modalTimerId),
+      20000
+   );
+
+   tabs(
+      ".tabheader__item",
+      ".tabcontent",
+      ".tabheader__items",
+      "tabheader__item_active"
+   );
+   modal("[data-modal]", ".modal", modalTimerId);
+   timer(".timer", "2023-10-30");
+   cards();
+   calc();
+   forms("form", modalTimerId);
+   slider({
+      container: ".offer__slider",
+      slide: ".offer__slide",
+      nextArrow: ".offer__slider-next",
+      prevArrow: ".offer__slider-prev",
+      totalCounter: "#total",
+      currentCounter: "#current",
+      wrapper: ".offer__slider-wrapper",
+      field: ".offer__slider-inner",
+   });
+});
+=======
 import tabs from './modules/tabs';
 import modal from './modules/modal';
 import timer from './modules/timer';
@@ -27,3 +70,4 @@ window.addEventListener('DOMContentLoaded', function() {
         field: '.offer__slider-inner'
     });
 });
+>>>>>>> 350eb14 (Add files via upload)
